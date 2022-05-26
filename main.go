c8b4ea1eefbf80ba0e0669715f3fbf36dd1afaaa
Michael Zuffoletti <mzuffoletti@llnw.com>
2022-05-25 17:57:42 -0700

    FindOne

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("MONGODB_URI missing from environnment, add it file .env")
+	}
+
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().
+		ApplyURI(uri).
+		SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	coll := client.Database("sample_mflix").Collection("movies")
+	title := "Back to the Future"
+
+	var result bson.M
+	err = coll.FindOne(ctx, bson.D{{"title", title}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		log.Printf("No document found with title %s", title)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", jsonData)
+}
